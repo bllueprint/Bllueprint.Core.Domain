364866517413923bd0daf34a4ec1426d187d1f98
Ulisses <ulisses.albino@remsoft.com>
2026-05-01 19:46:09 -0300

    Fixing some previous mistakes

--- src/ActionStep{T}.cs
+++ src/ActionStep{T}.cs
@@ -1,5 +1,5 @@
 namespace Bllueprint.Core.Domain;
 
-internal sealed record struct ActionStep<T>(Action<T> Action) : ITransitionStep<T>;
+internal record struct ActionStep<T>(Action<T> Action) : ITransitionStep<T>;
 
-internal file sealed record struct ActionStep<T, TArgs>(Action<T, TArgs> Action) : ITransitionStep<T, TArgs>;
+internal record struct ActionStep<T, TArgs>(Action<T, TArgs> Action) : ITransitionStep<T, TArgs>;
--- src/ITransitionStep{T}.cs
+++ src/ITransitionStep{T}.cs
@@ -1,5 +1,5 @@
 namespace Bllueprint.Core.Domain;
 
-internal file interface ITransitionStep<T>;
+internal interface ITransitionStep<T>;
 
 internal interface ITransitionStep<T, TArgs>;
